f2d205a18ac2f09dac2f0de3586502b971a7a775
johncburnett <john.burnett.c@gmail.com>
2016-06-02 12:11:15 -0500

    Added server template to cpp

---
+++ src/cpp/palo_duro/src/osc_server.cpp
@@ -0,0 +1,9 @@
+//
+//  osc_server.cpp
+//  palo_duro
+//
+//  Created by John Burnett on 6/2/16.
+//
+//
+
+#include "osc_server.hpp"
---
+++ src/cpp/palo_duro/src/osc_server.hpp
@@ -0,0 +1,14 @@
+//
+//  osc_server.hpp
+//  palo_duro
+//
+//  Created by John Burnett on 6/2/16.
+//
+//
+
+#ifndef osc_server_hpp
+#define osc_server_hpp
+
+#include <stdio.h>
+
+#endif /* osc_server_hpp */
