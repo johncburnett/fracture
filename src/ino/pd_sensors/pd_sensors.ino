dc42d454a54058ca555ae2a7de3ba85325a10f64
johncburnett <john.burnett.c@gmail.com>
2016-06-08 18:04:21 -0500

    Began implementing sensor read-in and OSC out

--- src/ino/pd_sensors/pd_sensors.ino
+++ src/ino/pd_sensors/pd_sensors.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * pd_sensors.ino - John Burnett + Sage Jenson (c) 2016
+ * 
+ * Based on CNMAT's UDPSendBundle.ino
+ */
 
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <SPI.h>    
+#include <OSCBundle.h>
+
+EthernetUDP Udp;
+
+//the Arduino's IP
+IPAddress ip(127, 0, 0, 1);
+//destination IP
+IPAddress outIp(127, 0, 0, 1);
+const unsigned int outPort = 12345;
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+
+void setup() {
+  Ethernet.begin(mac,ip);
+  Udp.begin(8888);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop(){
+    OSCBundle bndl;
+
+    bndl.add("/sensors/temperature").add((int32_t)analogRead(0)); // read from analog 0
+    bndl.add("/sensores/piezo").add((int32_t)analogRead(1)); // read from analog 1
+
+    Udp.beginPacket(outIp, outPort);
+    bndl.send(Udp);
+    Udp.endPacket();
+    bndl.empty();
+
+    delay(100);
 }
